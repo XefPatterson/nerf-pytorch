b5961bccb43339857df9109a6cd245a8f80de61d
Felix <felix.harvey@unity3d.com>
2021-03-23 13:23:50 -0400

    Added export script

---
+++ export_nerf.py
@@ -0,0 +1,188 @@
+import argparse
+import glob
+import os
+import time
+import ntpath
+
+import numpy as np
+import torch
+import torchvision
+import yaml
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm, trange
+
+from nerf import (CfgNode, get_embedding_function, get_ray_bundle, img2mse,
+                  load_blender_data, load_llff_data, meshgrid_xy, models,
+                  mse2psnr, run_one_iter_of_nerf)
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path to (.yml) config file."
+    )
+    parser.add_argument(
+        "--load-checkpoint",
+        type=str,
+        default="",
+        help="Path to load saved checkpoint from.",
+    )
+    configargs = parser.parse_args()
+
+    # Read config file.
+    cfg = None
+    with open(configargs.config, "r") as f:
+        cfg_dict = yaml.load(f, Loader=yaml.FullLoader)
+        cfg = CfgNode(cfg_dict)
+
+    # # (Optional:) enable this to track autograd issues when debugging
+    # torch.autograd.set_detect_anomaly(True)
+
+    # If a pre-cached dataset is available, skip the dataloader.
+    USE_CACHED_DATASET = False
+    train_paths, validation_paths = None, None
+    images, poses, render_poses, hwf, i_split = None, None, None, None, None
+    H, W, focal, i_train, i_val, i_test = None, None, None, None, None, None
+    if hasattr(cfg.dataset, "cachedir") and os.path.exists(cfg.dataset.cachedir):
+        train_paths = glob.glob(os.path.join(cfg.dataset.cachedir, "train", "*.data"))
+        validation_paths = glob.glob(
+            os.path.join(cfg.dataset.cachedir, "val", "*.data")
+        )
+        USE_CACHED_DATASET = True
+    else:
+        # Load dataset
+        images, poses, render_poses, hwf = None, None, None, None
+        if cfg.dataset.type.lower() == "blender":
+            images, poses, render_poses, hwf, i_split = load_blender_data(
+                cfg.dataset.basedir,
+                half_res=cfg.dataset.half_res,
+                testskip=cfg.dataset.testskip,
+            )
+            i_train, i_val, i_test = i_split
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            if cfg.nerf.train.white_background:
+                images = images[..., :3] * images[..., -1:] + (1.0 - images[..., -1:])
+        elif cfg.dataset.type.lower() == "llff":
+            images, poses, bds, render_poses, i_test = load_llff_data(
+                cfg.dataset.basedir, factor=cfg.dataset.downsample_factor
+            )
+            hwf = poses[0, :3, -1]
+            poses = poses[:, :3, :4]
+            if not isinstance(i_test, list):
+                i_test = [i_test]
+            if cfg.dataset.llffhold > 0:
+                i_test = np.arange(images.shape[0])[:: cfg.dataset.llffhold]
+            i_val = i_test
+            i_train = np.array(
+                [
+                    i
+                    for i in np.arange(images.shape[0])
+                    if (i not in i_test and i not in i_val)
+                ]
+            )
+            H, W, focal = hwf
+            H, W = int(H), int(W)
+            hwf = [H, W, focal]
+            images = torch.from_numpy(images)
+            poses = torch.from_numpy(poses)
+
+    # Seed experiment for repeatability
+    seed = cfg.experiment.randomseed
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # Device on which to run.
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    encode_position_fn = get_embedding_function(
+        num_encoding_functions=cfg.models.coarse.num_encoding_fn_xyz,
+        include_input=cfg.models.coarse.include_input_xyz,
+        log_sampling=cfg.models.coarse.log_sampling_xyz,
+    )
+
+    encode_direction_fn = None
+    if cfg.models.coarse.use_viewdirs:
+        encode_direction_fn = get_embedding_function(
+            num_encoding_functions=cfg.models.coarse.num_encoding_fn_dir,
+            include_input=cfg.models.coarse.include_input_dir,
+            log_sampling=cfg.models.coarse.log_sampling_dir,
+        )
+
+    # Initialize a coarse-resolution model.
+    model_coarse = getattr(models, cfg.models.coarse.type)(
+        num_encoding_fn_xyz=cfg.models.coarse.num_encoding_fn_xyz,
+        num_encoding_fn_dir=cfg.models.coarse.num_encoding_fn_dir,
+        include_input_xyz=cfg.models.coarse.include_input_xyz,
+        include_input_dir=cfg.models.coarse.include_input_dir,
+        use_viewdirs=cfg.models.coarse.use_viewdirs,
+    )
+    model_coarse.to(device)
+    # If a fine-resolution model is specified, initialize it.
+    model_fine = None
+    if hasattr(cfg.models, "fine"):
+        model_fine = getattr(models, cfg.models.fine.type)(
+            num_encoding_fn_xyz=cfg.models.fine.num_encoding_fn_xyz,
+            num_encoding_fn_dir=cfg.models.fine.num_encoding_fn_dir,
+            include_input_xyz=cfg.models.fine.include_input_xyz,
+            include_input_dir=cfg.models.fine.include_input_dir,
+            use_viewdirs=cfg.models.fine.use_viewdirs,
+        )
+        model_fine.to(device)
+
+    # Initialize optimizer.
+    trainable_parameters = list(model_coarse.parameters())
+    if model_fine is not None:
+        trainable_parameters += list(model_fine.parameters())
+    optimizer = getattr(torch.optim, cfg.optimizer.type)(
+        trainable_parameters, lr=cfg.optimizer.lr
+    )
+
+    # Setup logging.
+    logdir = os.path.join(cfg.experiment.logdir, cfg.experiment.id)
+    os.makedirs(logdir, exist_ok=True)
+    writer = SummaryWriter(logdir)
+    # Write out config parameters.
+    with open(os.path.join(logdir, "config.yml"), "w") as f:
+        f.write(cfg.dump())  # cfg, f, default_flow_style=False)
+
+    # By default, start at iteration 0 (unless a checkpoint is specified).
+    start_iter = 0
+
+    # Load an existing checkpoint, if a path is specified.
+    if os.path.exists(configargs.load_checkpoint):
+        checkpoint = torch.load(configargs.load_checkpoint, map_location=torch.device('cpu'))
+        model_coarse.load_state_dict(checkpoint["model_coarse_state_dict"])
+        if checkpoint["model_fine_state_dict"]:
+            model_fine.load_state_dict(checkpoint["model_fine_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+        dim_xyz = 3 + 2 * 3 * cfg.models.coarse.num_encoding_fn_xyz
+        dim_dir = 3 + 2 * 3 * cfg.models.coarse.num_encoding_fn_dir
+        dummy_input = torch.zeros((1, dim_dir + dim_xyz),dtype=torch.float)
+
+        out_folder, _ = ntpath.split(configargs.load_checkpoint)
+        torch.onnx.export(model_coarse, dummy_input, os.path.join(out_folder, "coarse_model.onnx"),
+                          verbose=False, input_names=["input"])
+        torch.onnx.export(model_fine, dummy_input, os.path.join(out_folder, "fine_model.onnx"), input_names=["input"])
+
+    print("Done!")
+
+
+def cast_to_image(tensor):
+    # Input tensor is (H, W, 3). Convert to (3, H, W).
+    tensor = tensor.permute(2, 0, 1)
+    # Conver to PIL Image and then np.array (output shape: (H, W, 3))
+    img = np.array(torchvision.transforms.ToPILImage()(tensor.detach().cpu()))
+    # Map back to shape (3, H, W), as tensorboard needs channels first.
+    img = np.moveaxis(img, [-1], [0])
+    return img
+
+
+if __name__ == "__main__":
+    main()
